4546a5bdcf42367a141347657c814f3ad5c9b710
Thomas Backmark <total@viskos.org>
2018-05-11 16:25:20 +0200

    Build update

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 echo Building Super Metroid + Zelda
 cd resources
 python create_exhirom.py sm_orig.sfc alttp_orig.sfc zsm_orig.sfc
-copy zsm_orig.sfc zsm.sfc > NUL:
+copy /y zsm_orig.sfc zsm.sfc > NUL:
 asar ..\src\main.asm zsm.sfc
 copy zsm.sfc "..\build\zsm.sfc" > NUL:
 cd ..
