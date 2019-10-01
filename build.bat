930e620eadb747403f6ded48cbf3c88887d3d966
Rebelus Quo <rebelusquo@tutanota.com>
2019-10-01 17:50:46 +0200

    Update asar to version 1.71
    Also add a bash shell script for scenarios such as git bash in git4win.

--- build.bat
+++ build.bat
@@ -1,9 +1,12 @@
 @echo off
+
 echo Building Super Metroid + Zelda
+
 cd resources
 python create_exhirom.py sm_orig.sfc alttp_orig.sfc zsm_orig.sfc
-copy /y zsm_orig.sfc zsm.sfc > NUL:
+copy /y zsm_orig.sfc zsm.sfc > NUL
 asar ..\src\main.asm zsm.sfc
-copy zsm.sfc "..\build\zsm.sfc" > NUL:
+copy zsm.sfc "..\build\zsm.sfc" > NUL
+
 cd ..
 echo Done
