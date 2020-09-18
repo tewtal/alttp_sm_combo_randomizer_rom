5dcf66db918f9dd4e718dfb016d0d0b66869a3df
SmolBig <smolbigrando@gmail.com>
2020-09-17 18:56:49 -0700

    Add keysanity door icons to SM maps

--- src/main.asm
+++ src/main.asm
@@ -27,6 +27,7 @@ incsrc "sm/newgame.asm"                     ; Super Metroid New Game Initializat
 incsrc "sm/minorfixes.asm"                  ; Super Metroid some softlock removals etc
 incsrc "sm/demofix.asm"                     ; Super Metroid Stop demos from playing
 incsrc "sm/maps.asm"                        ; Super Metroid map pause screen and HUD changes
+incsrc "sm/map_icons.asm"                   ; Super Metroid door icons for keysanity map
 incsrc "sm/max_ammo.asm"                    ; Super Metroid max ammo patch by personitis, adapted by Leno for Crossover
 incsrc "sm/multiworld.asm"                  ; Super Metroid Multiworld support features
 incsrc "sm/sprite/sprite.asm"               ; Super Metroid custom Samus sprite "engine" by Artheau
