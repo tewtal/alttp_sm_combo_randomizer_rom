39d933c75cce5c84ae5a0b08f1d8994a6a22f7ac
Thomas Backmark <total@viskos.org>
2022-02-20 22:28:47 +0100

    Include boss tokens in all dungeons requirements

--- src/z3/randomizer/goalitem.asm
+++ src/z3/randomizer/goalitem.asm
@@ -63,6 +63,7 @@ CheckGanonVulnerability:
 		;#$02 = Require All Dungeons
 		LDA $7EF374 : AND.b #$07 : CMP #$07 : BNE .fail ; require all pendants
 		LDA $7EF37A : AND.b #$7F : CMP #$7F : BNE .fail ; require all crystals
+		LDA !SRAM_SM_ITEM_BUF+$72 : AND.b #$0F : CMP #$0F : BNE .fail ; required all boss tokens
 		LDA $7EF3C5 : CMP.b #$03 : !BLT .fail ; require post-aga world state
 		LDA $7EF2DB : AND.b #$20 : CMP #$20 : BNE .fail ; require aga2 defeated (pyramid hole open)
 		BRA .success
