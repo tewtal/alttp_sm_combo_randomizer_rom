e04fbd6a390bde73886c38e30cc7b13ade5cccac
tewtal <total@viskos.org>
2023-07-08 17:14:32 +0200

    Fix issue with ALTTP starting equipment

--- src/z3/randomizer/events.asm
+++ src/z3/randomizer/events.asm
@@ -80,10 +80,10 @@ OnFileLoad:
 
 	LDA.b #$07 : STA $210C ; Restore screen 3 to normal tile area
 
-	LDA !FRESH_FILE_MARKER : BNE +
-		JSL.l OnNewFile
-		LDA.b #$FF : STA !FRESH_FILE_MARKER
-	+
+	; LDA !FRESH_FILE_MARKER : BNE +
+	; 	JSL.l OnNewFile
+	; 	LDA.b #$FF : STA !FRESH_FILE_MARKER
+	; +
 	LDA.w $010A : BNE + ; don't adjust the worlds for "continue" or "save-continue"
 	LDA.l $7EC011 : BNE + ; don't adjust worlds if mosiac is enabled (Read: mirroring in dungeon)
 		JSL.l DoWorldFix
@@ -108,29 +108,29 @@ RTL
 !RNG_ITEM_LOCK_IN = "$7F5090"
 OnNewFile:
 	PHX : PHP
-		REP #$20 ; set 16-bit accumulator
-		LDA.l LinkStartingRupees : STA $7EF362 : STA $7EF360
-		LDA.l StartingTime : STA $7EF454
-		LDA.l StartingTime+2 : STA $7EF454+2
+		REP #$20 ; set 16-bit accumulator		
+		LDA.l LinkStartingRupees : STA !SRAM_ALTTP_START+$362 : STA !SRAM_ALTTP_START+$360
+		LDA.l StartingTime : STA !SRAM_ALTTP_START+$454
+		LDA.l StartingTime+2 : STA !SRAM_ALTTP_START+$454+2
 
 		LDX.b #$00 : - ; copy over starting equipment
-			LDA.l StartingEquipment, X : STA $7EF340, X
+			LDA.l StartingEquipment, X : STA.l !SRAM_ALTTP_START+$340, X
 			INX : INX
 		CPX.b #$4F : !BLT -
 
 		SEP #$20 ; set 8-bit accumulator
 		;LDA #$FF : STA !RNG_ITEM_LOCK_IN ; reset rng item lock-in
 		LDA.l PreopenCurtains : BEQ +
-			LDA.b #$80 : STA $7EF061 ; open aga tower curtain
-			LDA.b #$80 : STA $7EF093 ; open skull woods curtain
+			LDA.b #$80 : STA !SRAM_ALTTP_START+$061 ; open aga tower curtain
+			LDA.b #$80 : STA !SRAM_ALTTP_START+$093 ; open skull woods curtain
 		+
 		LDA.l PreopenPyramid : BEQ +
-			LDA.b #$20 : STA $7EF2DB ; pyramid hole already open
+			LDA.b #$20 : STA !SRAM_ALTTP_START+$2DB ; pyramid hole already open
 		+
 		LDA.l PreopenGanonsTower : BEQ +
-			LDA.b #$20 : STA $7EF2C3 ; Ganons Tower already open
+			LDA.b #$20 : STA !SRAM_ALTTP_START+$2C3 ; Ganons Tower already open
 		+
-		LDA.l StartingSword : STA $7EF359 ; set starting sword type
+		LDA.l StartingSword : STA !SRAM_ALTTP_START+$359 ; set starting sword type
 	PLP : PLX
 RTL
 ;--------------------------------------------------------------------------------
